992a877e6a899db31c066bc330ca4407bb3aa815
ochecodes7 <ochecodes@gmail.com>
2023-01-18 22:58:51 +0100

    blog api

---
+++ Models/Author.cs
@@ -0,0 +1,8 @@
+namespace BlogApi.models 
+{
+    public class Author {
+       public int Id {get; set;}
+        public string Name {get; set;}
+        public string Description {get;set;}
+    }
+}
