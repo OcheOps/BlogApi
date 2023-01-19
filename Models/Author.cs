64415b1730fda458c3d26b518f116368dc4fe9e4
ochecodes7 <ochecodes@gmail.com>
2023-01-19 23:18:44 +0100

    fixed Author bug

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,7 +1,8 @@
-namespace BlogApi.models 
+namespace BlogApi.Models
 {
-    public class Author {
-       public int Id {get; set;}
+    public class Author
+    {
+        public int Id {get; set;}
         public string Name {get; set;}
         public string Description {get;set;}
     }
--- Models/Post.cs
+++ Models/Post.cs
@@ -1,8 +1,8 @@
-namespace BlogApi
+namespace BlogApi.Models
 {
-    public class BlogPost.Models
+    public class BlogPost
     {
-         public int Id {get; set;}
+        public int Id {get; set;}
         public string Title { get; set; }
         public string Summary { get; set; }
         public string Body { get; set; }
